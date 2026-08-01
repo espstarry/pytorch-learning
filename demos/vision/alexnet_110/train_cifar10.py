6b6a3d7e65224220b67d003f83bad9d0f188ae7f
espst <espst@example.com>
2026-08-01 14:37:05 +0800

    Add Hugging Face CIFAR-10 source

--- demos/vision/alexnet_110/train_cifar10.py
+++ demos/vision/alexnet_110/train_cifar10.py
@@ -2,7 +2,7 @@ import argparse
 
 import torch
 from torch import nn
-from torch.utils.data import DataLoader, Subset
+from torch.utils.data import DataLoader, Dataset, Subset
 from torchvision import datasets, transforms
 
 from model import AlexNet
@@ -13,12 +13,31 @@ parser.add_argument("--epochs", type=int, default=1)
 parser.add_argument("--batch-size", type=int, default=16)
 parser.add_argument("--train-samples", type=int, default=1000)
 parser.add_argument("--test-samples", type=int, default=200)
+parser.add_argument("--source", choices=["torchvision", "huggingface"], default="torchvision")
 args = parser.parse_args()
 
 # CIFAR-10 是 32×32 RGB；Resize 后符合经典 AlexNet 的 224×224 输入。
 transform = transforms.Compose([transforms.Resize(224), transforms.ToTensor()])
-train_set = datasets.CIFAR10("data", train=True, download=True, transform=transform)
-test_set = datasets.CIFAR10("data", train=False, download=True, transform=transform)
+
+if args.source == "torchvision":
+    train_set = datasets.CIFAR10("data", train=True, download=True, transform=transform)
+    test_set = datasets.CIFAR10("data", train=False, download=True, transform=transform)
+else:
+    from datasets import load_dataset
+
+    class HFCIFAR10(Dataset):
+        def __init__(self, split):
+            self.data = load_dataset("uoft-cs/cifar10", split=split)
+
+        def __len__(self):
+            return len(self.data)
+
+        def __getitem__(self, index):
+            sample = self.data[index]
+            return transform(sample["img"]), sample["label"]
+
+    train_set = HFCIFAR10("train")
+    test_set = HFCIFAR10("test")
 train_set = Subset(train_set, range(min(args.train_samples, len(train_set))))
 test_set = Subset(test_set, range(min(args.test_samples, len(test_set))))
 train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True)
